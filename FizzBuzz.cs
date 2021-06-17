aee7629cfdb02afd3da42db70fa8679296a18bda
kianniek <56929026+kianniek@users.noreply.github.com>
2021-06-17 20:03:16 +0200

    Create FizzBuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,28 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = 100;
+            string output = null;
+            for (int i = 1; i < length + 1; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    output = "FizzBuzz";
+                }
+                else if(i % 3 == 0)
+                {
+                    output = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    output = "Buzz";
+                }
+                else
+                {
+                    output =  i.ToString();
+                }
+                Console.WriteLine(output);
+            }
+        }
+    }
