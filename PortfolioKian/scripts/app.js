07a514797c20e267ac33235cba423a312394cf10
Kian <56929026+kianniek@users.noreply.github.com>
2023-01-30 19:06:45 +0100

    changed app.css and index.html and app.js

--- PortfolioKian/scripts/app.js
+++ PortfolioKian/scripts/app.js
@@ -5,6 +5,22 @@ let isMenuActive = false;
 
 const navButtonLinks = document.querySelectorAll('.navigation li a');
 
+const KianDOB = new Date("2004-04-30")
+
+window.onload = setAge(KianDOB)          
+
+function setAge(date){
+  let age = calculateAge(date);
+  document.getElementById('age').innerHTML = "Age: " +age ;  
+}
+
+function calculateAge(date)
+{
+  const now = new Date();
+  const diff = Math.abs(now - date );
+  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365)); 
+  return age;
+}
 navButtonLinks.forEach(link => {
   link.addEventListener('click', () => {
     const navIconMenu = `<i class="bi bi-list"></i>`;
@@ -32,6 +48,13 @@ navButton.addEventListener('click', () => {
   navMenu.classList.toggle('animate__bounceInLeft');
 });
 
+
+
+// Scroll Reveal
+ScrollReveal().reveal('.about', { delay: 300 });
+ScrollReveal().reveal('.testimonials', { delay: 300 });
+ScrollReveal().reveal('.contact', { delay: 300 });
+
 // Splidejs Slide
 const splide = new Splide('.splide', {
   type: 'loop',
@@ -56,8 +79,3 @@ const splide = new Splide('.splide', {
 });
 
 splide.mount();
-
-// Scroll Reveal
-ScrollReveal().reveal('.about', { delay: 300 });
-ScrollReveal().reveal('.testimonials', { delay: 300 });
-ScrollReveal().reveal('.contact', { delay: 300 });
