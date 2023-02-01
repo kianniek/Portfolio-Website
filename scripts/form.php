8c4e703a53564dcc64fa6dd0e9a5ff410d5bfc31
Kian <56929026+kianniek@users.noreply.github.com>
2023-02-01 15:07:49 +0100

    update php

--- scripts/form.php
+++ scripts/form.php
@@ -16,14 +16,7 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    function_alert()
-    //echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    echo 'alert("Mail Sent. Thank you " . $name . ", we will contact you shortly.")';
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-
-    function function_alert() { 
-              
-        // Display the alert box; note the Js tags within echo, it performs the magic
-        echo "<script>Mail Sent. Thank you " . $name . ", we will contact you shortly.</script>"; 
-    } 
 ?>
