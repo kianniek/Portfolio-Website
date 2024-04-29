2c390c850d89ce87445c59f562ac51af36973596
kian <kianniek@hotmail.com>
2024-04-29 17:12:29 +0200

    update to website testemonial

--- js/style-js.js
+++ js/style-js.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const carousel = document.querySelector('.carousel');
+  let isHovering = false; // Boolean to track hover state
+  const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
+	// Stop script if reduced motion is preferred
+  if (prefersReducedMotion) {
+    console.log("Reduced motion is enabled. Stopping the script.");
+    return; // Exit the function early
+  }
   // Helper functions to manipulate images
   function getAllImages() {
     return Array.from(carousel.querySelectorAll('img'));
@@ -16,9 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
   function adjustSpeed(newSpeed) {
     speed = newSpeed;
   }
+	
+  function lerp(start, end, amt) {
+    return (1-amt)*start+amt*end
+  }
 
   // Initialization of variables
-  let speed = 0.9; // Speed of the animation (pixels per millisecond)
+  let speed = 0; // Speed of the animation (pixels per millisecond)
+  let maxSpeed = 0.9;
   let spacing = 20; // Example initial spacing
   let images = getAllImages();
   let imagePositions = new Array(images.length).fill(0);
@@ -27,9 +39,23 @@ document.addEventListener("DOMContentLoaded", function () {
   carousel.querySelectorAll('img').forEach(img => {
     img.style.marginRight = `${spacing}vw`;
   });
+carousel.addEventListener('mouseover', () => {
+      isHovering = true;
+		console.log(isHovering);
+    });
+    carousel.addEventListener('mouseout', () => {
+      isHovering = false;
+    });
+  // Add hover event listeners to images
 
   // Animation function
   function animate() {
+	  if(isHovering){
+		  speed = lerp(speed, 0, 0.01);
+	  }else{
+		  speed = lerp(speed, maxSpeed, 0.01);
+	  }
+    
     let widthTotal = 0; // Total width of all images to calculate wrapping
     images.forEach((img, index) => {
       widthTotal += img.offsetWidth + parseInt(getComputedStyle(img).marginRight);
@@ -40,9 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
       img.style.transform = `translateX(${imagePositions[index]}px)`;
 
       let position = img.getBoundingClientRect();
-      if (position.left < 0) {
-        console.log("started exiting");
-      }
       if (position.right < 0) {
         // Wrap images when exiting screen
         wrapImage(index, widthTotal);
@@ -65,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Rearrange the images array for consistent handling
     const firstImage = images.shift();
     images.push(firstImage);
-    console.log(`Image wrapped: ${firstImage.src}`);
+    //console.log(`Image wrapped: ${firstImage.src}`);
   }
 
   animate(); // Start the animation
